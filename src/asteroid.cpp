058e5a15893635b943ea99bad3f6984e6f349384
Maximilian Wright <twixthehero@gmail.com>
2016-04-19 09:42:38 -0400

    Fixed gl included err

--- src/asteroid.cpp
+++ src/asteroid.cpp
@@ -1,4 +1,8 @@
 #include "asteroid.h"
+#include "gameobject.h"
+#include "gametime.h"
+#include "input.h"
+#include "transform.h"
 
 Asteroid::Asteroid()
 {
@@ -13,24 +17,24 @@ Asteroid::Asteroid(float speed)
 Asteroid::Asteroid(float speed, Transform* t)
 {
     moveSpeed = speed;
-    trans = t;
+    transform = t;
 }
 
 void Asteroid::update()
 {
-    trans->pos.y -= moveSpeed * GameTime::dt;
+    transform->pos.y -= moveSpeed * GameTime::dt;
     //std::cout << "asteroid: " << trans->pos.y << std::endl;
 }
-
+/*
 GameObject* Asteroid::createAsteroid(float speed)
 {
     //texturing
-    Texture* tex_missing = new Texture("missing.png");
+    Texture* tex_missing = TextureManager::instance->getTexture("missing.png");
     Material* mat_missing = new Material(EMaterialType::DEFAULT, ShaderManager::getDefaultShader(), tex_missing);
     //create game object
     GameObject* asteroid = new GameObject(vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1));
     Model* asteroidModel = new Model("asteroid");
-    MeshRenderer* asteroidRenderer = new MeshRenderer(asteroidModel, mat_missing);
+    MeshRenderer* asteroidRenderer = new MeshRenderer(EVertexFormat::XYZ_UV, asteroidModel, mat_missing);
     Asteroid* asteroidComponent = new Asteroid(speed);
     //add components
     asteroid->addComponent(asteroidRenderer);
@@ -39,15 +43,15 @@ GameObject* Asteroid::createAsteroid(float speed)
     return asteroid;
 }
 
-GameObject * Asteroid::createAsteroid(float speed, Transform* t)
+GameObject* Asteroid::createAsteroid(float speed, Transform* t)
 {
     //texturing
-    Texture* tex_missing = new Texture("missing.png");
+    Texture* tex_missing = TextureManager::instance->getTexture("missing.png");
     Material* mat_missing = new Material(EMaterialType::DEFAULT, ShaderManager::getDefaultShader(), tex_missing);
     //create game object
     GameObject* asteroid = new GameObject(vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1));
     Model* asteroidModel = new Model("asteroid");
-    MeshRenderer* asteroidRenderer = new MeshRenderer(asteroidModel, mat_missing);
+    MeshRenderer* asteroidRenderer = new MeshRenderer(EVertexFormat::XYZ_UV, asteroidModel, mat_missing);
     Asteroid* asteroidComponent = new Asteroid(speed, t);
     //add components
     asteroid->addComponent(asteroidRenderer);
@@ -55,3 +59,4 @@ GameObject * Asteroid::createAsteroid(float speed, Transform* t)
     //return
     return asteroid;
 }
+*/
